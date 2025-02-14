6dbe9ff4842cd6f268beb6c1ba9273d46bf3659d
Sandesh-itsme <neupanesandesh620@gmail.com>
2025-02-14 20:32:26 +0545

    Array file added

---
+++ arrays_13.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main(){
+  // 1. Minimum element of an array
+  int arr[5];
+  cout<<"Enter the elements : \n";
+  for(int i=0;i<5;i++){
+    cin>>arr[i];
+  }
+  cout<<"\nThe elements are : \n";
+  for(int i=0;i<5;i++){
+    cout<<arr[i]<<"\t";
+  }
+  int min=arr[0];
+  for(int i=0;i<5;i++){
+    if(arr[i]<min){
+        min=arr[i];
+    }
+  }
+  cout<<"\nThe minimum element is : "<<min<<endl;
+  int max=arr[0];
+  for(int i=0;i<5;i++){
+    if(arr[i]>max){
+        max=arr[i];
+    }
+  }
+  cout<<"The maximum element is : "<<max<<endl;
+
+
+
+
+    return 0;
+}
